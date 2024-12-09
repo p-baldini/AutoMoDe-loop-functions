3dfe06d55161cb3a68bbc9892e7668d22da3bb47
Paolo Baldini <paolo.baldini.phd@gmail.com>
2024-12-09 13:01:46 +0100

    added support control in lahmacun loop functions

---
+++ loop-functions/lahmacun/SteadyRobot.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file <lahmacun/SteadyRobot.cpp>
+ * 
+ * @author Paolo Baldini - <paolo.baldini.phd@gmail.com>
+ * 
+ * @package ARGoS3-AutoMoDe
+ * 
+ * @license MIT License
+ */
+#include "SteadyRobot.hpp"
+
+#include <algorithm>
+
+/***********************************************/
+/***********************************************/
+
+void SteadyRobot::Init(TConfigurationNode& t_node)
+{
+    // set the wheel speed to zero to keep the robot still
+    auto wheels = GetActuator<CCI_EPuckWheelsActuator>("epuck_wheels");
+    wheels->SetLinearVelocity(0, 0);
+
+    // save the pointers to the range and bearing sensor and actuator; save the pointer to the leds
+    // actuator
+    rab_actuator = GetActuator<CCI_EPuckRangeAndBearingActuator>("epuck_range_and_bearing");
+    rab_sensor = GetSensor<CCI_EPuckRangeAndBearingSensor>("epuck_range_and_bearing");
+    led_actuator = GetActuator<CCI_EPuckRGBLEDsActuator>("epuck_rgb_leds");
+
+    // get the robot id from its name (max 6 characters)
+    robot_id = atoi(GetId().substr(7, 6).c_str());
+}
+
+/***********************************************/
+/***********************************************/
+
+void SteadyRobot::ControlStep()
+{
+    // get the rab messages received
+    auto messages = rab_sensor->GetPackets();
+
+    // check if at least a message has the repair content
+    auto checker = [this](auto o) { return o->Data[0] != 0; };
+    bool received_repair = std::any_of(messages.begin(), messages.end(), checker);
+    rab_sensor->ClearPackets();
+
+    // if the robot should answer repair messages, then answer
+    if (received_repair && answer_messages)
+    {
+        UInt8 message[] = { robot_id, message_id++, 1, 1 };
+        rab_actuator->SetData(message);
+        answered = true;
+    }
+    else
+    {
+        answered = false;
+    }
+
+    // set the led color as dictated by the loop function
+    led_actuator->SetColors(current_color);
+}
+
+/***********************************************/
+/***********************************************/
+
+void SteadyRobot::Reset()
+{
+    
+}
+
+/***********************************************/
+/***********************************************/
+
+void SteadyRobot::Destroy()
+{
+    
+}
+
+/***********************************************/
+/***********************************************/
+
+REGISTER_CONTROLLER(SteadyRobot, "lahmacun_steady_controller")
---
+++ loop-functions/lahmacun/SteadyRobot.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file <lahmacun/SteadyRobot.hpp>
+ * 
+ * @author Paolo Baldini - <paolo.baldini.phd@gmail.com>
+ * 
+ * @package ARGoS3-AutoMoDe
+ * 
+ * @license MIT License
+ */
+#ifndef LAHMACUN_STEADY_ROBOT_HPP
+#define LAHMACUN_STEADY_ROBOT_HPP
+
+#include <argos3/core/utility/math/rng.h>
+#include <argos3/core/control_interface/ci_controller.h>
+#include <argos3/plugins/robots/e-puck/control_interface/ci_epuck_wheels_actuator.h>
+#include <argos3/plugins/robots/e-puck/control_interface/ci_epuck_range_and_bearing_actuator.h>
+#include <argos3/plugins/robots/e-puck/control_interface/ci_epuck_range_and_bearing_sensor.h>
+#include <argos3/plugins/robots/e-puck/control_interface/ci_epuck_rgb_leds_actuator.h>
+
+using namespace argos;
+
+/**
+ * A controller for a robot which is part of the experiment environment. It is a robot that stays
+ * still and assumes different states, represented by its leds color. A loop-function is expected
+ * to set the robot color and decide wether it should answer incomin RAB messages. When the robot
+ * answers a RAB message, it set a variable to notify the loop function. The answer contains the
+ * robot ID, the message ID, and 1s.
+ */
+class SteadyRobot : public CCI_Controller {
+    public:
+        /**
+         * Init the robot by getting its sensors and actuators and by setting its ID.
+         * 
+         * @param[in] t_node Points to the <parameters> section in the XML file in the
+         * <controllers><lahmacun-steady-robot> section.
+         */
+        virtual void Init(TConfigurationNode& t_node);
+
+        /**
+         * Set the leds color and possibly answer incoming messages.
+         */
+        virtual void ControlStep();
+
+        /**
+         * Currently, this method does nothing.
+         */
+        virtual void Reset();
+
+        /**
+         * Currently, this method does nothing.
+         */
+        virtual void Destroy();
+
+        CColor current_color;                           ///< The current color of the robot leds.
+        bool answer_messages;                           ///< True if the robot should answer
+                                                        ///< incoming messages, false otherwise. 
+        bool answered;                                  ///< True if the robot answered a message
+                                                        ///< at the previous step.
+
+    private:
+        UInt8 robot_id;                                 ///< The ID of the robot in argos.
+        UInt8 message_id;                               ///< Incremental counter containing the ID
+                                                        ///< of the message: it avoids perceiving
+                                                        ///< messages from other sensors
+                                                        ///< considering them different.
+
+        CCI_EPuckRangeAndBearingActuator* rab_actuator; ///< Pointer to the range and bearing
+                                                        ///< actuator.
+        CCI_EPuckRangeAndBearingSensor* rab_sensor;     ///< Pointer to the range and bearing
+                                                        ///< sensor.
+        CCI_EPuckRGBLEDsActuator* led_actuator;         ///< Pointer to the leds actuator.
+};
+
+#endif /* LAHMACUN_STEADY_ROBOT_HPP */
